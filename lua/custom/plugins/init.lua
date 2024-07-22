0cc9feee3d858bd3b1c37bbaacf8c1ca36c16e3d
quang.phan <ptquang2000@gmail.com>
2024-07-22 22:07:59 +0700

    mod theme

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -34,7 +34,7 @@ return {
             " Set contrast.
             " This configuration option should be placed before `colorscheme gruvbox-material`.
             " Available values: 'hard', 'medium'(default), 'soft'
-            let g:gruvbox_material_background = 'hard'
+            let g:gruvbox_material_background = 'soft'
             " For better performance
             let g:gruvbox_material_better_performance = 1
             let g:gruvbox_material_enable_italic = 1
